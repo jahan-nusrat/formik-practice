1332adfb070a52fa3a3d9900e755a58cdc2f77a0
Nusrat Jahan <njnusrat53@gmail.com>
2020-12-27 19:45:02 +0600

    register done

--- src/components/Registration/RegistrationForm.js
+++ src/components/Registration/RegistrationForm.js
@@ -1,10 +1,81 @@
+import { Form, Formik } from 'formik'
 import React from 'react'
+import * as Yup from 'yup'
+import Formikontrol from '../Formikontrol'
 
 const RegistrationForm = () => {
+    const mocOptions = [
+        {key: 'Email', value:'mocEmail'},
+        {key: 'Telephone', value:'mocTelephone'}
+    ]
+    const initialValues = {
+        email: '',
+        password : '',
+        confirmPassword : '',
+        modeOfContact: '',
+        phone : ''
+    }
+    const validationSchema = Yup.object({
+        email: Yup.string()
+            .email('Invalid Email Address')
+            .required('Required!'),
+        password : Yup.string().required('Required!'),
+        confirmPassword : Yup.string()
+            .oneOf([Yup.ref('password'),''], 'Password must match')
+            .required('Required!'),
+        modeOfContact: Yup.string().required('Required!'),
+        phone: Yup.string().when('modeOfContact' , {
+            is : 'mocTelephone',
+            then : Yup.string().required('Required')
+        })
+    })
+    const onSubmit = values =>{
+        console.log('Form data', values)
+    }
     return (
-        <div>
-            <h3>Hello</h3>
-        </div>
+        <Formik
+        initialValues = {initialValues}
+        validationSchema = {validationSchema}
+        onSubmit = {onSubmit}
+        >
+            {
+                formik => (
+                    <Form>
+                        <Formikontrol
+                            control='input'
+                            type='email'
+                            name= 'email'
+                            label = 'Email'
+                        />
+                        <Formikontrol
+                            control='input'
+                            type='password'
+                            name= 'password'
+                            label = 'Password'
+                        />
+                        <Formikontrol
+                            control='input'
+                            type='password'
+                            name= 'confirmPassword'
+                            label = 'Confirm Password'
+                        />
+                        <Formikontrol
+                            control='radio'
+                            name= 'modeOfContact'
+                            label = 'Mode of contact'
+                            options = {mocOptions}
+                        />
+                        <Formikontrol
+                            control='input'
+                            type='text'
+                            name= 'phone'
+                            label = 'Phone'
+                        />
+                        <button type='submit' disabled={!formik.isValid}>Register</button>
+                    </Form>
+                )
+            }
+        </Formik>
     )
 }
 
